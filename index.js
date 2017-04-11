458e4174210d64ebc4f2163149ca80632fe56fe1
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-04-11 15:24:44 -0400

    image card example

--- index.js
+++ index.js
@@ -1,104 +1,237 @@
-'use strict'; 
- var http = require ('http');
+'use strict';
 
-exports.handler = function (event,context) {
-    var request  = event.request;
-try {
-if (request.type === "LaunchRequest") {
-    var options = {};
-    options.speechText = "Hi my name is millie i'm the family doggie, using alexa i can talk but in real life i cannot see, i can see but i can not talk, tell alexa who is here. ";
-    options.repromptText = "For example, like say hi to ronak";
-    options.endSession = false;  
-    context.succeed(buildResponse(options));
+var http = require('http');
 
-} else if (request.type === "IntentRequest"){
-    let options = {};
-   
-    if (request.intent.name === "HelloIntent"){
-        
-        let name = request.intent.slots.FirstName.value;
-        // options.speechText = `woof <say-as interpret-as="spell-out">${name}</say-as> ${name}. `;
-        options.speechText = "woof, Hi " +name+ " .";
-        options.speechText += getWish();
-        getQuote(function(quote,err){
-            if(err) {
-                context.fail(err);
-            } else {
-                options.speechText += quote;
-                options.endSession = true;
-                context.succeed(buildResponse(options));
-            }
-        });
-        
+exports.handler = function(event,context) {
+
+  try {
+
+    if(process.env.NODE_DEBUG_EN) {
+      console.log("Request:\n"+JSON.stringify(event,null,2));
+    }
+
+
+
+    var request = event.request;
+    var session = event.session;
+
+    if(!event.session.attributes) {
+      event.session.attributes = {};
+    }
+
+    /*
+      i)   LaunchRequest       Ex: "Open greeter"
+      ii)  IntentRequest       Ex: "Say hello to John" or "ask greeter to say hello to John"
+      iii) SessionEndedRequest Ex: "exit" or error or timeout
+    */
+
+    if (request.type === "LaunchRequest") {
+      handleLaunchRequest(context);
+
+    } else if (request.type === "IntentRequest") {
+
+      if (request.intent.name === "HelloIntent") {
+
+        handleHelloIntent(request,context);
+
+      } else if (request.intent.name === "QuoteIntent") {
+
+        handleQuoteIntent(request,context,session);
+
+      } else if (request.intent.name === "NextQuoteIntent") {
+
+        handleNextQuoteIntent(request,context,session);
+
+      } else if (request.intent.name === "AMAZON.StopIntent" || request.intent.name === "AMAZON.CancelIntent") {
+        context.succeed(buildResponse({
+          speechText: "Good bye. ",
+          endSession: true
+        }));
+
+      } else {
+        throw "Unknown intent";
+      }
+
+    } else if (request.type === "SessionEndedRequest") {
+
     } else {
-       throw "Unknown HelloIntent request";
+      throw "Unknown intent type";
     }
-} else if (request.type === "SessionEndedRequest") {
+  } catch(e) {
+    context.fail("Exception: "+e);
+  }
 
-} else {
-    throw "Unknown intent type";
- } 
-} catch(e) {
-    context.fail("Exception: " +e);
-    }
 }
 
-function getQuote (callback) {
-    var url = "http://api.forismatic.com/api/1.0/json?method=getQuote&lang=en&format=json";
-    var req = http.get(url,function(res){
-        var body = "";
+function getQuote(callback) {
+  var url = "http://api.forismatic.com/api/1.0/json?method=getQuote&lang=en&format=json";
+  var req = http.get(url, function(res) {
+    var body = "";
 
-        res.on('data',function(chunk){
-            body += chunk;
-        });
-
-        res.on('end', function(){
-            body = body.replace(/\\/g,'');
-            var quote = JSON.parse(body);
-            callback(quote.quoteText); // define what parameter you want from the api like quoteText  
-        });
-    }); 
-
-    req.on('error', function(err){
-        callback('', err);
+    res.on('data', function(chunk) {
+      body += chunk;
     });
+
+    res.on('end', function() {
+      body = body.replace(/\\/g,'');
+      var quote = JSON.parse(body);
+      callback(quote.quoteText);
+    });
+
+  });
+
+  req.on('error', function(err) {
+    callback('',err);
+  });
+  
 }
 
-function getWish(){
-    var myDate = new Date();
-    var hours = myDate.getUTCHours() - 5;
-    if (hours < 0) {
-        hours = hours + 24;
-    }
+function getWish() {
+  var myDate = new Date();
+  var hours = myDate.getUTCHours() - 8;
+  if (hours < 0) {
+    hours = hours + 24;
+  }
 
-    if(hours < 12) {
-        return "Woof Morning, Is it Time for Breakfast? As some human once said ";
-    } else if (hours < 18) {
-        return "Woof afternoon, Is it Time for Lunch? As some human once said ";
-    } else {
-        return "Woof evening, Is it Time for Dinner? As some human once said ";
-    }
+  if (hours < 12) {
+    return "Woof Morning, i will tell you quote for a treat, So as some human once said ";
+  } else if (hours < 18) {
+    return "Woof Afternoon, i will tell you quote for a treat, So as some human once said ";
+  } else {
+    return "Woof Night, i will tell you quote for a treat, So as some human once said ";
+  }
+  
 }
+
 
 function buildResponse(options) {
-    var response = {
-        version: "1.0",
-        response:{
-            outputSpeech: {
-                type: "SSML",
-                ssml: "<speak>"+options.speechText+"</speak>"
-            },
-            shouldEndSession: options.endSession
-        }
-    };
 
-    if(options.repromptText){
-        response.response.reprompt = {
-            outputSpeech: {
-                type: "SSML",
-                ssml: "<speak>"+options.speechText+"</speak>"  
-            }
-        };
+  if(process.env.NODE_DEBUG_EN) {
+    console.log("buildResponse options:\n"+JSON.stringify(options,null,2));
+  }
+
+  var response = {
+    version: "1.0",
+    response: {
+      outputSpeech: {
+        type: "SSML",
+        ssml: "<speak>"+options.speechText+"</speak>"
+      },
+      shouldEndSession: options.endSession
     }
-    return response;
+  };
+
+  if(options.repromptText) {
+    response.response.reprompt = {
+      outputSpeech: {
+        type: "SSML",
+        ssml: "<speak>"+options.repromptText+"</speak>"
+      }
+    };
+  }
+
+  if(options.cardTitle) {
+    response.response.card = {
+      type: "Simple",
+      title: options.cardTitle
+    }
+
+    if(options.imageUrl) {
+      response.response.card.type = "Standard";
+      response.response.card.text = options.cardContent;
+      response.response.card.image = {
+        smallImageUrl: options.imageUrl,
+        largeImageUrl: options.imageUrl
+      };
+
+    } else {
+      response.response.card.content = options.cardContent;
+    }
+  }
+
+
+
+
+  if(options.session && options.session.attributes) {
+    response.sessionAttributes = options.session.attributes;
+  }
+
+  if(process.env.NODE_DEBUG_EN) {
+    console.log("Response:\n"+JSON.stringify(response,null,2));
+  }
+
+  return response;
+}
+
+function handleLaunchRequest(context) {
+  let options = {};
+  options.speechText =  "Hi my name is millie i'm the family doggie, using computer i can finally talk. Want to listen to a quote? Tell me your name?";
+  options.repromptText = "You can say for example, say John. ";
+  options.endSession = false;
+  context.succeed(buildResponse(options));
+}
+
+function handleHelloIntent(request,context) {
+  let options = {};
+  let name = request.intent.slots.FirstName.value;
+  options.speechText = `Whooof <say-as interpret-as="spell-out">${name}</say-as> ${name}. `;
+  options.speechText += getWish();
+
+  options.cardTitle = `Wooof ${name}!`;
+
+  getQuote(function(quote,err) {
+    if(err) {
+      context.fail(err);
+    } else {
+      options.speechText += quote;
+      options.cardContent = quote;
+      options.imageUrl = "https://www.flickr.com/photos/dainec/15674465189";
+      options.endSession = true;
+      context.succeed(buildResponse(options));
+    }
+  });
+}
+
+function handleQuoteIntent(request,context,session) {
+  let options = {};
+  options.session = session;
+
+  getQuote(function(quote,err) {
+    if(err) {
+      context.fail(err);
+    } else {
+      options.speechText = quote;
+      options.speechText += " Do you want to listen to one more quote? ";
+      options.repromptText = "You can say yes or one more. ";
+      options.session.attributes.quoteIntent = true;
+      options.endSession = false;
+      context.succeed(buildResponse(options));
+    }
+  });
+
+}
+
+function handleNextQuoteIntent(request,context,session) {
+  let options = {};
+  options.session = session;
+
+  if(session.attributes.quoteIntent) {
+    getQuote(function(quote,err) {
+      if(err) {
+        context.fail(err);
+      } else {
+        options.speechText = quote;
+        options.speechText += " Do you want to listen to one more quote? ";
+        options.repromptText = "You can say yes or one more. ";
+        //options.session.attributes.quoteIntent = true;
+        options.endSession = false;
+        context.succeed(buildResponse(options));
+      }
+    });
+  } else {
+    options.speechText = " Wrong invocation of this intent. ";
+    options.endSesstion = true;
+    context.succeed(buildResponse(options));
+  }
+
 }
